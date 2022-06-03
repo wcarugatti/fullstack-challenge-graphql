d0979c39068d7a4972b968a0de29750db09a8cc3
Wilson Carugatti <wilson.tibia.valentin@gmail.com>
2022-06-03 20:15:30 -0300

    feat: insert seed data on the init.sql

--- backend/src/infra/postgres/resources/init.sql
+++ backend/src/infra/postgres/resources/init.sql
@@ -32,8 +32,30 @@ CREATE TABLE IF NOT EXISTS "subscription" (
   "created_at" VARCHAR(255),
   "updated_at" VARCHAR(255),
   "deleted_at" VARCHAR(255),
-  PRIMARY KEY ("id", "customer_id", "plan_id", "payment_gateway_id"),
+  PRIMARY KEY (
+    "id",
+    "customer_id",
+    "plan_id",
+    "payment_gateway_id"
+  ),
   FOREIGN KEY ("customer_id") REFERENCES "customer" (id),
   FOREIGN KEY ("plan_id") REFERENCES "plan" (id),
   FOREIGN KEY ("payment_gateway_id") REFERENCES "payment_gateway" (id)
 );
+
+INSERT INTO
+  "plan"
+VALUES
+  ('plan1_1', 'The Creative', 1, 29.00),
+  ('plan1_2', 'The Creative', 2, 278.00),
+  ('plan2_1', 'Adventurer', 1, 108.00),
+  ('plan2_2', 'Adventurer', 2, 1231.20),
+  ('plan3_1', 'Super Star', 1, 325.00),
+  ('plan3_2', 'Super Star', 2, 3705.00)
+
+INSERT INTO
+  "payment_gateway"
+VALUES
+  ('pg1', 'Stripe'),
+  ('pg2', 'PayPal')
+  
