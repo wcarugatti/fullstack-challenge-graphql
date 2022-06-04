ca8be5245da277d741ade5a5688c8ba8c1b4628c
Wilson Carugatti <wilson.tibia.valentin@gmail.com>
2022-06-04 00:47:00 -0300

    build: configure apollo graphql server

---
+++ backend/src/main/graphql/ApolloServerHandler.ts
@@ -0,0 +1,24 @@
+import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import { Express } from "express";
+import http from "http";
+
+export class ApolloServerHandler {
+  constructor(private app: Express) {}
+
+  async setupServer(typeDefs, resolvers, port: number) {
+    const httpServer = http.createServer(this.app);
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      csrfPrevention: true,
+      plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    });
+    await server.start();
+    server.applyMiddleware({ app: this.app });
+    await new Promise<void>((resolve) =>
+      httpServer.listen({ port }, resolve),
+    );
+    console.log(`Running on port ${port}`);
+  }
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,14 @@
 import "dotenv/config";
-import { app } from "./app";
+import { app } from "./main/express/app";
 import "reflect-metadata";
 import { sequelize } from "./infra/postgres/helper";
+import { ApolloServerHandler } from "./main/graphql/ApolloServerHandler";
+import typeDefs from "./main/graphql/typeDefs";
+import resolvers from "./main/graphql/resolvers";
 
 (async () => {
   await sequelize.authenticate();
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  const apolloServer = new ApolloServerHandler(app);
+  const PORT = +process.env.PORT || 5000;
+  await apolloServer.setupServer(typeDefs, resolvers, PORT);
 })();
