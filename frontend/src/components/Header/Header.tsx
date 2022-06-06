693ff44ea3cc981ae1ebe2a2a2b022668913bd9f
Wilson Carugatti <wilson.tibia.valentin@gmail.com>
2022-06-06 07:56:54 -0300

    feat: create header details

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,11 +1,18 @@
+import { KeyboardArrowDownOutlined } from "@mui/icons-material";
 import { Toolbar } from "@mui/material";
-import { HeaderAppBar } from "./styles";
+import { CustomChip, HeaderAppBar } from "./styles";
 const Header: React.FC = () => {
   return (
-    <HeaderAppBar
-      position="static"
-    >
-      <Toolbar></Toolbar>
+    <HeaderAppBar position="static">
+      <Toolbar>
+        <CustomChip
+          variant="outlined"
+          color="primary"
+          label="Hi Nour"
+          deleteIcon={<KeyboardArrowDownOutlined />}
+          onDelete={() => {}}
+        />
+      </Toolbar>
     </HeaderAppBar>
   );
 };
--- frontend/src/components/Header/styles.ts
+++ frontend/src/components/Header/styles.ts
@@ -1,13 +1,23 @@
 import styled from "styled-components";
-import { AppBar } from "@mui/material";
-
-export const Container = styled.div`
-  height: 60px;
-  box-shadow: 1px;
-`;
+import { AppBar, Chip } from "@mui/material";
 
 export const HeaderAppBar = styled(AppBar)`
   background-color: #fff;
   z-index: 0;
   box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%);
+  .MuiToolbar-root {
+    justify-content: end;
+  }
+`;
+
+export const CustomChip = styled(Chip)`
+  color: var(--soft-black);
+  border-color: var(--primary);
+  border-width: 2px;
+  font-weight: 600;
+  padding-right: 7px;
+  padding-left: 7px;
+  svg {
+    fill: var(--soft-black);
+  }
 `;
