de7a54c7c1a922045fd0a2716c35784f703def18
Wilson Carugatti <wilson.tibia.valentin@gmail.com>
2022-06-06 08:14:08 -0300

    fix: remove unused dependency

--- frontend/src/components/CustomerForm/styles.ts
+++ frontend/src/components/CustomerForm/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Button, Card, Grid, TextField } from "@mui/material";
+import { Button, Card, Grid } from "@mui/material";
 
 export const Container = styled.div`
   padding: 40px;
