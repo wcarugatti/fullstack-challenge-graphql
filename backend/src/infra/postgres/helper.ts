6be78dfc4ae6ed0bd5b8290329152e4aab7f1f8b
Wilson Carugatti <wilson.tibia.valentin@gmail.com>
2022-06-06 09:06:44 -0300

    fix: fix the load of sequelize models

--- backend/src/infra/postgres/helper.ts
+++ backend/src/infra/postgres/helper.ts
@@ -1,10 +1,15 @@
 import { Sequelize } from "sequelize-typescript";
+import Customer from "./models/Customer.model";
+import PaymentGateway from "./models/PaymentGateway.model";
+import Plan from "./models/Plan.model";
+import Subscription from "./models/Subscription.model";
 
 export const sequelize = new Sequelize({
   database: process.env.POSTGRES_DB,
-  dialect: 'postgres',
+  dialect: "postgres",
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   host: process.env.POSTGRES_HOST,
-  models: [__dirname + '/**/*.model.ts'] 
-})
+});
+
+sequelize.addModels([Customer, PaymentGateway, Plan, Subscription]);
